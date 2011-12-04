35747ba0e5f1c67273bd432bd373df57704abd99
Sören Hentzschel <cadeyrn@Cadeyrns-Macbook-Pro.local>
2011-12-04 16:23:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fhboerse::Application.config.secret_token = '68031b9a6fc01454cdc4dc16c08c7727f8bb715bec7b7b1b85c4ff6253640ae79ff089b9e0bcdaa2ee23bc07a99f23cded3ffe45d9f70f3d1493d831ead588d3'
