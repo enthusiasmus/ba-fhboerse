66c630e728b86b75e5627b9d17cd225aaf2f5e6f
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2011-12-28 17:55:52 +0100

    changed validators of job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,17 +2,25 @@ class Job < ActiveRecord::Base
   belongs_to :user
   attr_accessible :paid, :salary, :employment_status, :company, :begindate, :place, :qualification, :title, :description, :forename, :lastname, :email, :telephone, :offer_or_quest
 
-  validates_numericality_of :salary, :on => :create, :message => "^Geben Sie bitte als Bezahlung nur Ziffern von 0-9 an!"
+  validates_length_of :title, :within => 5..100, :too_short => "^Der Titel muss mehr als 5 Zeichen besitzen!", :too_long => "Der Titel muss weniger als 100 Zeichen besitzen!"
+  validates_length_of :description, :within => 30..1000, :too_short => "^Die Beschreibung muss mehr als 30 Zeichen besitzen!", :too_long => "Die Beschreibung muss weniger als 1000 Zeichen besitzen!"
+  validates_length_of :qualification, :within => 10..1000, :too_short => "^Die Qualifikationsbeschreibung muss mehr als 10 Zeichen besitzen!", :too_long => "Die Qualifikationsbeschreibung muss weniger als 1000 Zeichen besitzen!"
 
-  validates_inclusion_of :paid, :in => [true, false], :message => "^Geben Sie bitte die Art der Arbeit an!"
-  validates_presence_of :lastname, :message => "^Geben Sie bitte die Art der Arbeit an!"
-  validates_presence_of :email, :message => "^Geben Sie bitte ihre E-Mail-Adresse an!"
-  validates_presence_of :telephone, :message => "^Geben Sie bitte ihr Telefonnummer an!"
-  validates_inclusion_of :offer_or_quest, :in => [true, false], :message => "^Geben Sie bitte den Typ der Annonce an!"
-  validates_presence_of :begindate, :message => "^Geben Sie bitte ein Datum fuer den Begin der Arbeit an!"
-  validates :agb, :acceptance => true
+  validates_presence_of :place, :message => "^Bitte geben Sie den Arbeitsort an"
+  validates_presence_of :begindate, :message => "^Bitte geben Sie den Arbeitsbegin an!"
   
-  validates_length_of :title, :within => 4..50, :too_short => "^Der Titel muss mehr als 4 Zeichen besitzen!", :too_long => "Der Titel muss weniger als 50 Zeichen besitzen!"
-  validates_length_of :description, :within => 30..1500, :too_short => "^Die Beschreibung muss mehr als 30 Zeichen besitzen!", :too_long => "Die Beschreibung muss weniger als 1500 Zeichen besitzen!"
-  validates_length_of :qualification, :within => 10..1500, :too_short => "^Die Beschreibung muss mehr als 10 Zeichen besitzen!", :too_long => "Die Beschreibung muss weniger als 1500 Zeichen besitzen!"
+  if :paid == "true"
+    validates_numericality_of :salary, :on => :create, :message => "^Bitte geben Sie das Gehalt an (Ziffern von 0-9)!"
+  end
+
+  if :company != ""
+    validates_presence_of :lastname, :message => "^Bitte geben Sie den Nachnamen an!"
+  else
+    validates_presence_of :company, :message => "^Bitte geben Sie die Firma an!"
+  end
+  
+  validates_presence_of :email, :message => "^Bitte geben Sie ihre E-Mail-Adresse an!"
+  validates_presence_of :telephone, :message => "^Bitte geben Sie ihr Telefonnummer an!"
+
+  validates :agb, :acceptance => true
 end
