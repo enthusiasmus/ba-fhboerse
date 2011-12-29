1dc88596784a53100625eda9a84dd526f16de412
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2011-12-29 01:04:41 +0100

    added precompiling

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Fhboerse
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.precompile += ['jobs.css']
   end
 end
