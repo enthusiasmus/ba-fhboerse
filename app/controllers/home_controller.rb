51b4638f343481289c59244fd97b73018b89f859
Sören Hentzschel <cadeyrn@Cadeyrns-Macbook-Pro.local>
2012-01-12 07:39:56 +0100

    changed home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,5 +15,6 @@ class HomeController < ApplicationController
     @my_items = Item.order("created_at DESC").where("user_id = '#{session[:user_id]}'")
     
     @my_articles = (@my_jobs + @my_drives + @my_products + @my_apartments + @my_items).sort_by(&:created_at).reverse!
+    @latest_articles = (@newest_jobs + @newest_drives + @newest_products + @newest_apartments + @newest_items).sort_by(&:created_at).reverse!
   end
 end
